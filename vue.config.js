d3779baa1a59d68c8ba28206ba0129c92cd7ba35
Isaac Ndala <isaacndala111@gmail.com>
2022-08-16 15:59:20 +0100

    Add pwa configuration in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: "Climatic",
+    themeColor: '#4787b1',
+    msTileColor: '#000000',
+    iconPaths: {
+      faviconSVG: 'img/icons/logo.png',
+      favicon32: 'img/icons/logo.png',
+      favicon16: 'img/icons/logo.png',
+      appleTouchIcon: 'img/icons/logo.png',
+      maskIcon: 'img/icons/logo.svg',
+      msTileImage: 'img/icons/logo.png'
+    }
+  }
 })
